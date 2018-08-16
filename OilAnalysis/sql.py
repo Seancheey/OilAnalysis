945aaf50f556fef327b48a0296c2c9da50dfd018
Seancheey <adls371@163.com>
2018-08-17 00:53:48 +0800

    add gen_create_query

--- OilAnalysis/sql.py
+++ OilAnalysis/sql.py
@@ -1,33 +1,30 @@
 import sqlalchemy.engine
-from OilAnalysis.settings import local_engine
+from OilAnalysis.settings import engine
 
 __type_map = {int: "int", float: "float", str: "text"}
 
 
 def insert_values(engine_or_connection, table_name: str, entries):
 	if type(entries) == dict:
-		engine_or_connection.execute(__gen_insert_query(table_name, entries))
+		engine_or_connection.execute(gen_insert_query(table_name, entries))
 	elif type(entries) == list:
 		for entry in entries:
-			engine_or_connection.execute(__gen_insert_query(table_name, entry))
+			engine_or_connection.execute(gen_insert_query(table_name, entry))
 	else:
 		raise TypeError("entries with type %s is not supported" % type(entries))
 
 
-def has_table(engine_or_connection: sqlalchemy.engine.Engine, table_name: str, schema=None) -> bool:
-	return engine_or_connection.has_table(table_name, schema=schema)
-
-
 def create_table(engine: sqlalchemy.engine.Engine, table_name: str, sample_entry: dict, suffix: dict = None):
-	table_columns = []
-	types = __get_sql_types(sample_entry)
-	for col, val in sample_entry.items():
-		suf = suffix[col] if col in suffix else ""
-		table_columns.append("%s %s %s" % (col, types[col], suf))
-	engine.execute("CREATE TABLE IF NOT EXISTS %s(%s)" % (table_name, ",".join(table_columns)))
+	query = gen_create_query(table_name, __get_sql_types(sample_entry), suffix)
+	engine.execute(query)
 
 
-def __gen_insert_query(table_name: str, values: dict):
+def gen_create_query(table_name, column_types: dict, column_suffix: dict = None):
+	entries = ["%s %s %s" % (c, t, column_suffix[c] if c in column_suffix else "") for c, t in column_types.items()]
+	return "CREATE TABLE IF NOT EXISTS %s(%s)" % (table_name, ",".join(entries))
+
+
+def gen_insert_query(table_name: str, values: dict):
 	columns = ",".join(values.keys())
 	col_values = ",".join([str(value) if type(value) != str else "'%s'" % value for value in values.values()])
 	return "INSERT INTO %s(%s) VALUES (%s);" % (table_name, columns, col_values)
@@ -44,8 +41,8 @@ def __get_sql_types(sample_entry) -> dict:
 
 
 def __unit_test():
-	create_table(local_engine, "sql_test", {"id": 123, "title": "iraq", "excerpt": "yoobuddy"}, {"id": "primary key"})
-	insert_values(local_engine, "sql_test", {"id": 123, "title": "iraq", "excerpt": "yoobuddy"})
+	create_table(engine, "sql_test", {"id": 123, "title": "iraq", "excerpt": "yoobuddy"}, {"id": "primary key"})
+	insert_values(engine, "sql_test", {"id": 123, "title": "iraq", "excerpt": "yoobuddy"})
 
 
 if __name__ == "__main__":
