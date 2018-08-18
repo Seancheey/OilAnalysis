fd944a61c294a304e29543032b624c6f0fdc005c
Seancheey <adls371@163.com>
2018-08-19 01:54:31 +0800

    revise pipeline

--- OilAnalysis/pipelines.py
+++ OilAnalysis/pipelines.py
@@ -5,8 +5,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from OilAnalysis.settings import engine
-from OilAnalysis.spiders.oilnews import OilNewsSpider
-from OilAnalysis.spiders.oil_daily_price import OilDailyPriceSpider
 from OilAnalysis.sqlsettings import *
 from datetime import datetime
 from abc import ABC, abstractmethod
@@ -24,22 +22,22 @@ class SQLExportPipeline(ABC):
 
 	@property
 	@abstractmethod
-	def target_spider(self):
+	def target_spider_name(self):
 		pass
 
 	def __init__(self):
 		self.connection = engine.connect()
 
 	def open_spider(self, spider):
-		if type(spider) == self.target_spider and not engine.has_table(self.settings.table_name):
+		if spider.name == self.target_spider_name and not engine.has_table(self.settings.table_name):
 			self.connection.execute(self.settings.gen_create_query())
 
 	def close_spider(self, spider):
-		if type(spider) == self.target_spider:
+		if spider.name == self.target_spider_name:
 			self.connection.close()
 
 	def process_item(self, item, spider):
-		if type(spider) != self.target_spider:
+		if spider.name != self.target_spider_name:
 			return item
 		item = self.pre_process_item(item)
 		self.connection.execute(self.settings.insert_query(item))
@@ -52,7 +50,7 @@ class SQLExportPipeline(ABC):
 
 class OilNewsPipeline(SQLExportPipeline):
 	settings = oil_news_settings
-	target_spider = OilNewsSpider
+	target_spider_name = "oilnews"
 
 	def pre_process_item(self, item):
 		# format publish_time to standard datetime before insert
@@ -64,7 +62,7 @@ class OilNewsPipeline(SQLExportPipeline):
 
 class OilDailyPricePipeline(SQLExportPipeline):
 	settings = oil_daily_price_settings
-	target_spider = OilDailyPriceSpider
+	target_spider_name = "oil_daily_price"
 
 	def pre_process_item(self, item):
 		# format last update
