4978abcfd6da569dd37607e0b62ca0ba4ffe82b6
sean shan <adls371@outlook.com>
2018-08-22 10:26:11 -0400

    reformat pipeline

--- OilAnalysis/pipelines.py
+++ OilAnalysis/pipelines.py
@@ -8,58 +8,62 @@ from OilAnalysis.settings import engine
 from OilAnalysis.sqlsettings import *
 from datetime import datetime
 from abc import ABC, abstractmethod
+from scrapy.exceptions import CloseSpider
 
 
 # Aug 16, 2018, 11:00 AM CDT
 
 class SQLExportPipeline(ABC):
-	__slots__ = "connection"
+    __slots__ = "connection"
 
-	@property
-	@abstractmethod
-	def settings(self) -> SQLSettings:
-		pass
+    @property
+    @abstractmethod
+    def settings(self) -> SQLSettings:
+        pass
 
-	@property
-	@abstractmethod
-	def target_spider_name(self):
-		pass
+    @property
+    @abstractmethod
+    def target_spider_name(self):
+        pass
 
-	def __init__(self):
-		self.connection = engine.connect()
+    def __init__(self):
+        self.connection = engine.connect()
 
-	def open_spider(self, spider):
-		if spider.name == self.target_spider_name and not engine.has_table(self.settings.table_name):
-			self.connection.execute(self.settings.gen_create_query())
+    def open_spider(self, spider):
+        if spider.name == self.target_spider_name and not engine.has_table(self.settings.table_name):
+            self.connection.execute(self.settings.gen_create_query())
 
-	def close_spider(self, spider):
-		if spider.name == self.target_spider_name:
-			self.connection.close()
+    def close_spider(self, spider):
+        if spider.name == self.target_spider_name:
+            self.connection.close()
 
-	def process_item(self, item, spider):
-		if spider.name != self.target_spider_name:
-			return item
-		item = self.pre_process_item(item)
-		self.connection.execute(self.settings.insert_query(item))
-		return item
+    def process_item(self, item, spider):
+        if spider.name != self.target_spider_name:
+            return item
+        item = self.pre_process_item(item)
+        self.connection.execute(self.settings.insert_query(item))
+        return item
 
-	@abstractmethod
-	def pre_process_item(self, item):
-		return item
+    def close_spider(self):
+        raise CloseSpider("")
+
+    @abstractmethod
+    def pre_process_item(self, item):
+        return item
 
 
 class OilNewsPipeline(SQLExportPipeline):
-	settings = oil_news_settings
-	target_spider_name = "oilnews"
+    settings = oil_news_settings
+    target_spider_name = "oilnews"
 
-	def pre_process_item(self, item):
-		return item
+    def pre_process_item(self, item):
+        return item
 
 
 class OilDailyPricePipeline(SQLExportPipeline):
-	settings = oil_daily_price_settings
-	target_spider_name = "oil_daily_price"
+    settings = oil_daily_price_settings
+    target_spider_name = "oil_daily_price"
 
-	def pre_process_item(self, item):
-		# format last update
-		return item
+    def pre_process_item(self, item):
+        # format last update
+        return item
