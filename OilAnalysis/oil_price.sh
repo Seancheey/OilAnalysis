49c9784ece51843ff0efd68bda919b8039f9bd6f
sean shan <adls371@outlook.com>
2018-09-10 17:52:57 -0400

    add network check

--- OilAnalysis/oil_price.sh
+++ OilAnalysis/oil_price.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /Users/seancheey/Documents/workspace/Python/OilAnalysis/
-/usr/local/anaconda3/envs/global_env/bin/python -m OilAnalysis.spiders.oil_daily_price
+wget -q -O /dev/null google.com
+if [ $? = 0 ]
+then
+    /usr/local/anaconda3/envs/global_env/bin/python -m OilAnalysis.spiders.oil_daily_price
+else
+    echo network not avaliable
+fi
