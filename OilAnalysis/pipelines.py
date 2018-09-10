a14c7e6871c4da6f25fd5388a0065f9118068723
sean shan <adls371@outlook.com>
2018-09-10 00:19:32 -0400

    remove unused package

--- OilAnalysis/pipelines.py
+++ OilAnalysis/pipelines.py
@@ -6,7 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from OilAnalysis.settings import engine
 from OilAnalysis.sqlsettings import *
-from datetime import datetime
 from abc import ABC, abstractmethod
 from scrapy.exceptions import CloseSpider
 
