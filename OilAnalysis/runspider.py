b796dc069c3cf768c4a9d622ba3387f276404df6
sean shan <adls371@outlook.com>
2018-10-13 15:30:45 -0400

    add error message

--- OilAnalysis/runspider.py
+++ OilAnalysis/runspider.py
@@ -1,8 +1,13 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from twisted.internet.error import DNSLookupError
+from datetime import datetime
 
 
 def run(spider_cls):
-	process = CrawlerProcess(get_project_settings())
-	process.crawl(spider_cls)
-	process.start()
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(spider_cls)
+    try:
+        process.start()
+    except DNSLookupError:
+        print("Error At", datetime.now(), ": DNS lookup failed")
