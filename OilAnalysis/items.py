e19963afcc2f0351d30590fd311b74cca08d07e0
Qiyi Shan <adls371@outlook.com>
2019-09-24 21:05:37 -0400

    handle possible date parse bug

--- OilAnalysis/items.py
+++ OilAnalysis/items.py
@@ -11,14 +11,22 @@ from scrapy.loader.processors import Join, TakeFirst
 from datetime import datetime
 
 
+def _process_oil_news_time(strings):
+    for s in strings:
+        try:
+            return datetime.strptime(" ".join(s.split()[:-1]), "- %b %d, %Y, %I:%M %p")
+        except(ValueError, IndexError):
+            pass
+    raise ValueError(strings + ' is not parsable')
+
+
 class NewsItem(Item):
     title = scrapy.Field(output_processor=TakeFirst())
     author = scrapy.Field(output_processor=TakeFirst())
     content = scrapy.Field(input_processor=Join("\n"), output_processor=TakeFirst())
-    publish_date = scrapy.Field(
-        input_processor=lambda x: datetime.strptime(" ".join(x[1].split()[:-1]), "- %b %d, %Y, %I:%M %p"),
-        output_processor=lambda x: x[0].isoformat().replace("T", " ")
-    )
+    publish_date = scrapy.Field(input_processor=_process_oil_news_time,
+                                output_processor=lambda x: x[0].isoformat().replace("T", " ")
+                                )
     reference = scrapy.Field(output_processor=TakeFirst())
 
 
