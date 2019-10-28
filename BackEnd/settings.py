1819e0d0ef0d989e11d2e05351f10500302f7cfe
Qiyi Shan <adls371@outlook.com>
2019-10-28 18:15:52 -0400

    switch to production database

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -3,4 +3,4 @@ from sqlalchemy import create_engine
 __local_engine = create_engine("mysql+pymysql://oil:h1VHhQWour@localhost:3306/oil_analysis")
 test_engine = create_engine("mysql+pymysql://oil:h1VHhQWour@localhost:3306/test")
 
-engine = test_engine
+engine = __local_engine
