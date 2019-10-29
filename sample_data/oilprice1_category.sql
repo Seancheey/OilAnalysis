6172da8a646134b82f0b4ad549174c4d7ebb14ab
Qiyi Shan <adls371@outlook.com>
2019-10-29 19:22:09 -0400

    add sample oil price data

---
+++ sample_data/oilprice1_category.sql
@@ -0,0 +1,6 @@
+INSERT INTO test.oil_price_categories (category_id, category_name) VALUES (4, 'Canadian Blends');
+INSERT INTO test.oil_price_categories (category_id, category_name) VALUES (1, 'Futures & Indexes');
+INSERT INTO test.oil_price_categories (category_id, category_name) VALUES (3, 'International Prices');
+INSERT INTO test.oil_price_categories (category_id, category_name) VALUES (6, 'OPEC Members');
+INSERT INTO test.oil_price_categories (category_id, category_name) VALUES (2, 'OPEC Members (Daily Pricing)');
+INSERT INTO test.oil_price_categories (category_id, category_name) VALUES (5, 'United States Blends');
