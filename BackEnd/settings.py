74e1b2713e03ab62cae1c6af8b008ab93f753a05
Qiyi Shan <adls371@outlook.com>
2019-11-02 00:56:43 -0400

    add Base to settings.

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-__local_engine = create_engine("mysql+pymysql://oil:h1VHhQWour@localhost:3306/oil_analysis")
-test_engine = create_engine("mysql+pymysql://oil:h1VHhQWour@localhost:3306/test")
+__sql_url = "mysql+pymysql://oil:h1VHhQWour@localhost:3306/"
+__production_schema = 'oil_analysis'
+__test_schema = 'oil_analysis_test'
 
-engine = __local_engine
+# choose between production schema and test schema
+schema = __production_schema
+
+engine = create_engine(__sql_url + schema)
+
+Base = declarative_base(bind=engine)
