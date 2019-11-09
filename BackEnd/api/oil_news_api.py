75681e0a971b0b66478abf505e55b26587e1a5f1
Qiyi Shan <adls371@outlook.com>
2019-11-08 20:11:09 -0500

    fix news item not bounded to session bug

--- BackEnd/api/oil_news_api.py
+++ BackEnd/api/oil_news_api.py
@@ -4,11 +4,13 @@ from BackEnd.api.utils import new_session
 from BackEnd.objects import OilNews
 
 
+def __wrap_oil_news(news: OilNews) -> OilNews:
+    return OilNews(news_id=news.news_id, title=news.title, publish_date=news.publish_date, author=news.author,
+                   content=news.content, reference=news.reference, retrieve_time=news.retrieve_time)
+
+
 def get_oil_news_list(start_time: datetime = None, end_time: datetime = None, news_num: int = -1) -> list:
     """
-    >>> get_oil_news_list(start_time=datetime(year=2018,month=9,day=20)) is not None
-    True
-
     get oil news within certain range of time. (not required)
 
     :param start_time: optional
@@ -24,19 +26,26 @@ def get_oil_news_list(start_time: datetime = None, end_time: datetime = None, ne
             result = result.filter(OilNews.publish_date < end_time)
         if news_num > 0:
             result = result.limit(news_num)
-        return [OilNews(news_id=news.news_id, title=news.title, publish_date=news.publish_date, author=news.author,
-                        content=news.content, reference=news.reference, retrieve_time=news.retrieve_time) for news in
-                result]
+        result = result.all()
+        return [__wrap_oil_news(news) for news in result]
 
 
 def get_one_oil_news(news_id: int) -> OilNews:
     """
-
     fetch a single oil news
 
     :param news_id: required, id for news
     :return: OilNews object
     """
     with new_session() as session:
-        news = session.query(OilNews).filter(OilNews.news_id == news_id).one_or_none()
-        return news
+        return __wrap_oil_news(session.query(OilNews).filter(OilNews.news_id == news_id).one_or_none())
+
+
+def get_latest_news() -> OilNews:
+    """
+    fetch latest news
+
+    :return: OilNews object
+    """
+    with new_session() as session:
+        return __wrap_oil_news(session.query(OilNews).order_by(OilNews.publish_date).limit(1).one_or_none())
